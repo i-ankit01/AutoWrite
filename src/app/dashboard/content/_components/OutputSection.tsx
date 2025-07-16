ded9fd801f62d21b022f7e8c789ab91174d3e341
i-ankit01 <ankit.common.acc@gmail.com>
2025-07-16 22:07:32 +0530

    copy implement

--- src/app/dashboard/content/_components/OutputSection.tsx
+++ src/app/dashboard/content/_components/OutputSection.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "@toast-ui/editor/dist/toastui-editor.css";
 import { Editor } from "@toast-ui/react-editor";
 import { Button } from "@/components/ui/button";
-import { Copy } from "lucide-react";
+import { Check, Copy } from "lucide-react";
 
 interface props {
   aiOutput : string;
@@ -10,6 +10,7 @@ interface props {
 
 const OutputSection = ({aiOutput} : props) => {
   const editorRef: any = useRef(null);
+  const [isCopied, setIsCopied] = useState(false);
 
   useEffect(()=>{
     const editorInstance = editorRef.current.getInstance()
@@ -20,9 +21,16 @@ const OutputSection = ({aiOutput} : props) => {
     <div className="bg-white shadow-md rounded-lg w-200 mt-7">
       <div className="flex justify-between items-center p-5">
         <h2 className="font-semibold text-primary text-lg">Output</h2>
-        <Button className="cursor-pointer">
-          {" "}
-          <Copy /> Copy{" "}
+        <Button className="cursor-pointer"
+        onClick={()=>{navigator.clipboard.writeText(aiOutput)
+          setIsCopied(true);
+
+          setTimeout(()=>{
+            setIsCopied(false)
+          },1000)
+        }}
+        >
+          {isCopied ? <Check /> : <Copy />} {isCopied ? "Copied" : "Copy"}
         </Button>
       </div>
       <Editor
